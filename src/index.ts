d8b0e7c3d8fdaf3a596eca542d0d41112e964ca2
jprice <jprice@izeni.com>
2021-10-25 00:39:42 -0600

    Add lit-html dep and web components polyfill

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,11 @@
+// Polyfill for extending HTMLElement
+import '@webcomponents/custom-elements/src/native-shim';
+// Polyfill for supporting browsers that do not know custom elements
+import '@webcomponents/custom-elements/custom-elements.min';
+
+export { html, render } from 'lit-html';
+
 export * from './app';
+export * from './component';
 export * from './http';
 export * from './router';
