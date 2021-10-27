d560b5286221e714d14b114ac80a138a45efa5e6
jprice <jprice@izeni.com>
2021-10-27 17:14:41 -0600

    Read DEBUG in from process.env

--- src/util.ts
+++ src/util.ts
@@ -1,6 +1,6 @@
 // Switch to true if you want to be spammed relentlessly.
 // This is for development purposes.
-export const DEBUG = false;
+export const DEBUG = process.env.DEBUG === 'true';
 
 export function logIfDebug(...args: any[]) {
   if (DEBUG) {
